60ac42927ddbad1d05a3a41e4ca1e2df95fed3cb
AngeLopez <angel@angelopez.ca>
2015-07-21 14:17:07 -0400

    Adding cocoon and rewards functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def show
+    @user = User.find(paramd[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+   @user = User.new(user_params)
+      if @user.save
+        redirect_to products_url, notice: "Signed up!"
+      else
+        render "new"
+      end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to projects_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
