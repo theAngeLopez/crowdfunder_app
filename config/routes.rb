538303f769db167255c9c07bf03bd3a67bed8079
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-07-22 16:17:05 -0400

    fixed bug in rewards

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
-  resources :users
   resources :sessions
 
   root 'pages#homepage'
