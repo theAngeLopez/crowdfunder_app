5e5aa1360a82b697f0c95b8c33caedb653b94f7e
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-07-24 10:21:22 -0400

    completed categories for second time

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,15 +1,34 @@
 $(document).on("ready page:load", function(){
+
+    $('#Business').hide();
+    $('#Education').hide();
+    $('#Recreation').hide();
+    $('#Charity').hide();
+
+
   $('.my_button').click(function() {
     userInput = ($(this).attr("value"));
     console.log(userInput);
       if (userInput == "Business"){
-        alert("Business");
+        $('#Business').show();
+        $('#Education').hide();
+        $('#Recreation').hide();
+        $('#Charity').hide();
       } else if (userInput == "Education") {
-        alert("Education");
+        $('#Business').hide();
+        $('#Education').show();
+        $('#Recreation').hide();
+        $('#Charity').hide();
       } else if (userInput == "Recreation") {
-        alert("Recreation");
+        $('#Business').hide();
+        $('#Education').hide();
+        $('#Recreation').show();
+        $('#Charity').hide();
       } else if (userInput == "Charity") {
-        alert("Charity");
+        $('#Business').hide();
+        $('#Education').hide();
+        $('#Recreation').hide();
+        $('#Charity').show();
       }
   })
 })
